1209ed723630c2ef32381b396eb8894a142beaa2
benzatman <benjaminzatman@gmail.com>
2022-08-23 09:01:18 -0400

    bug fixes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-webhook_app = Flask('JOCChatbotMiddleman')
+webhook_app = Flask(__name__.split('.')[0])
 
 # import the endpoint definitions from routes.py
 from app import routes
